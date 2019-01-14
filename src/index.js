4637c41047a0f5c4967991fd6cfe0c992be0f68d
Jose Villarroel <oscarclarke9900@gmail.com>
2019-01-14 11:34:38 -0400

    fixed email and checking activities

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM  from 'react-dom';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (failure) => console.log(failure)
-        );
-    return <div>Hello world</div>
+class App extends Component {
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(
+            position => console.log(position),
+            failure => console.log(failure)
+            );
+    }
+
+    render(){
+        return <div>Latitude: </div>
+    }
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
