f340b4a9df900e14a42bd69148c84c0ba0365e85
Jose Villarroel <oscarclarke9900@gmail.com>
2019-01-14 12:19:23 -0400

    testing mail fix

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import ReactDOM  from 'react-dom';
 
 class App extends Component {
+    state = {
+        test: 0
+    }
     componentDidMount(){
         window.navigator.geolocation.getCurrentPosition(
             position => console.log(position),
